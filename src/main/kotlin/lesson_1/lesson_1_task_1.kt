7d19dc09ae05f6136ad6fa7d456fc2b9a049d5dd
AndrewKh-2022 <a65har@rambler.ru>
2024-01-27 16:55:41 +0300

    KS-1-1 Task 1 completed I hope

---
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -0,0 +1,11 @@
+package lesson_1
+
+fun main(){
+
+    val numberOfApplication: Int = 75
+    val messageText: String = "Благодарим за покупку!"
+
+    println("Количество зявок: $numberOfApplication")
+    println("Текст в чеке - $messageText")
+
+}
