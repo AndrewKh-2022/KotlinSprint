e3c1180be70804c55c8c2c672a4fc6abee8cdde4
AndrewKh-2022 <a65har@rambler.ru>
2024-02-01 20:03:19 +0300

    KS-2-4 Task 4 of lesson 2 completed, fix #1

--- src/main/kotlin/lesson_24/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_24/lesson_2_task_4.kt
@@ -3,12 +3,12 @@ package lesson_24
 fun main(){
     val ironOreMain = 11.0
     val crystalOreMain = 7.0
-    val baff = 0.2
     val ironOreBaff: Int
     val crystalOreBaff: Int
+    val percentBaff = 20.0
 
-    ironOreBaff = (ironOreMain * baff).toInt()
-    crystalOreBaff = (crystalOreMain * baff).toInt()
+    ironOreBaff = (ironOreMain * percentBaff / 100).toInt()
+    crystalOreBaff = (crystalOreMain * percentBaff / 100).toInt()
 
     println("Дополнительное количество железной руды:\t\t$ironOreBaff")
     println("Дополнительное количество кристаллической руды:\t$crystalOreBaff")
